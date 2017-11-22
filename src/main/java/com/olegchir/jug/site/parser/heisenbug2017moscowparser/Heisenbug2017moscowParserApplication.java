a32b3418d3fa4d536e30cea4a2248c71ae49cd08
Oleg Chirukhin <olegchiruhin@gmail.com>
2017-11-23 06:54:21 +0700

    initial commit

---
+++ src/main/java/com/olegchir/jug/site/parser/heisenbug2017moscowparser/Heisenbug2017moscowParserApplication.java
@@ -0,0 +1,12 @@
+package com.olegchir.jug.site.parser.heisenbug2017moscowparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heisenbug2017moscowParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heisenbug2017moscowParserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/olegchir/jug/site/parser/heisenbug2017moscowparser/Heisenbug2017moscowParserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olegchir.jug.site.parser.heisenbug2017moscowparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Heisenbug2017moscowParserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
